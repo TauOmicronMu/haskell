e62d2e064cb8815a7bf83c2792972be3f64facfb
tauomicronmu <tauomicronmurs@gmail.com>
2016-10-14 00:28:31 +0100

    further work on decode

--- morse/unassessed3.hs
+++ morse/unassessed3.hs
@@ -6,8 +6,23 @@ import MorseLib
    and a mediumGap between each word. -}
 encode :: String -> [MorseUnit]
 encode [] = []
-encode xs = concat [codeWord (toUpper `map` w) ++ shortGap | w <- (words xs)]
+encode xs = concat [codeWord (toUpper `map` w) ++ mediumGap | w <- (words xs)]
 
 codeWord :: String -> [MorseUnit]
 codeWord []     = []
-codeWord (x:xs) = codeSymbol x ++ mediumGap ++ codeWord xs 
+codeWord (x:xs) = codeSymbol x ++ shortGap ++ codeWord xs
+
+{- Take an array of Beeps/Silences and produce an array of
+   dits, dahs, shortGaps and mediumGaps. -}
+morseWord :: [MorseUnit] -> [[MorseUnit]] 
+morseWord (Beep:Silence:xs)                    = [dit] ++ morseWord xs
+morseWord (Beep:Beep:Beep:Silence:xs)          = [dah] ++ morseWord xs 
+morseWord (Silence:Silence:Silence:Silence:xs) = [mediumGap] ++ morseWord xs
+morseWord (Silence:Silence:xs)                 = [shortGap] ++ morseWord xs
+morseWord _                                    = []
+
+{- Concatenate all of the lists that are either side of each
+   mediumGap to form the whole morse letters. -}
+morseLetters :: [[MorseUnit]] -> [[MorseUnit]]
+
+-- [[B,S],[B,B,B,S],[S,S,S,S],[S,S]] -> [[B,S,B,B,B,S],[S,S,S,S],[S,S]]
