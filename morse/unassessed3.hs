381a7987f88b9a30510c57f6596716220c381259
tauomicronmu <tauomicronmurs@gmail.com>
2016-10-14 14:05:01 +0100

    Finished implementation of decode

--- morse/unassessed3.hs
+++ morse/unassessed3.hs
@@ -1,5 +1,6 @@
 import Data.Char
 
+import ListSplit
 import MorseLib
 
 {- Encode a string into Morse, with a shortGap after each letter
@@ -12,17 +13,37 @@ codeWord :: String -> [MorseUnit]
 codeWord []     = []
 codeWord (x:xs) = codeSymbol x ++ shortGap ++ codeWord xs
 
+
+decode :: [MorseUnit] -> String
+decode xs = parsePhrase $ morseLetters $ morseWords $ morseUnits xs
+ 
 {- Take an array of Beeps/Silences and produce an array of
-   dits, dahs, shortGaps and mediumGaps. -}
-morseWord :: [MorseUnit] -> [[MorseUnit]] 
-morseWord (Beep:Silence:xs)                    = [dit] ++ morseWord xs
-morseWord (Beep:Beep:Beep:Silence:xs)          = [dah] ++ morseWord xs 
-morseWord (Silence:Silence:Silence:Silence:xs) = [mediumGap] ++ morseWord xs
-morseWord (Silence:Silence:xs)                 = [shortGap] ++ morseWord xs
-morseWord _                                    = []
+   tokens representing dits, dahs, shortGaps and mediumGaps. -}
+morseUnits :: [MorseUnit] -> [String] 
+morseUnits (Beep:Silence:xs)                    = ["dit"] ++ morseUnits xs
+morseUnits (Beep:Beep:Beep:Silence:xs)          = ["dah"] ++ morseUnits xs 
+morseUnits (Silence:Silence:Silence:Silence:xs) = ["mediumGap"] ++ morseUnits xs
+morseUnits (Silence:Silence:xs)                 = ["shortGap"] ++ morseUnits xs
+morseUnits _                                    = []
 
-{- Concatenate all of the lists that are either side of each
-   mediumGap to form the whole morse letters. -}
-morseLetters :: [[MorseUnit]] -> [[MorseUnit]]
+morseWords :: [String] -> [[String]]
+morseWords xs = splitWhen (=="mediumGap") xs
 
--- [[B,S],[B,B,B,S],[S,S,S,S],[S,S]] -> [[B,S,B,B,B,S],[S,S,S,S],[S,S]]
+morseLetters :: [[String]] -> [[[String]]]
+morseLetters xs = init [splitWhen (=="shortGap") ys | ys <- xs] -- init to remove the trailing silence.
+
+{- Inverse of morseUnits -}
+morseList :: [String] -> [MorseUnit]
+morseList ("dit":xs)       = dit ++ morseList xs
+morseList ("dah":xs)       = dah ++ morseList xs
+morseList ("mediumGap":xs) = mediumGap ++ morseList xs
+morseList ("shortGap":xs)  = shortGap ++ morseList xs
+morseList _                = [] 
+
+parsePhrase ::  [[[String]]] -> String
+parsePhrase []     = ""
+parsePhrase (x:xs) = parseWord x ++ " " ++ parsePhrase xs
+    where 
+        parseWord :: [[String]] -> String
+        parseWord xs = [l | x <- xs, (c,l) <- table, morseList x == c] 
+
